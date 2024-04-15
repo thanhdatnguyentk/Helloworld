253c5b4f429a7b25d00a467583a6056f776c90f2
thanhdatnguyentk <89179761+thanhdatnguyentk@users.noreply.github.com>
2024-04-15 15:15:12 +0000

    tuan 4 oop 2

--- OOP/BTH4_NguyenThanhDat_23520261/Bai03/MangSoNguyen.cpp
+++ OOP/BTH4_NguyenThanhDat_23520261/Bai03/MangSoNguyen.cpp
@@ -1,26 +1,25 @@
-#include<bits/stdc++.h>
-#include"MangSoNguyen.h"
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include "MangSoNguyen.h"
 
-int main(){
+int main() {
     int kichthuoc1, kichthuoc2;
-    cout << "Nhập kích thước mảng thứ nhất: ";
-    cin >> kichthuoc1;
+    std::cout << "Nhập kích thước mảng thứ nhất: ";
+    std::cin >> kichthuoc1;
     MangSoNguyen arr1(kichthuoc1);
-    cout << "Nhập mảng thứ nhất: ";
-    cin >> arr1;
+    std::cout << "Nhập mảng thứ nhất: ";
+    std::cin >> arr1;
 
-    cout << "Nhập kích thước mảng thứ hai: ";
-    cin >> kichthuoc2;
+    std::cout << "Nhập kích thước mảng thứ hai: ";
+    std::cin >> kichthuoc2;
     MangSoNguyen arr2(kichthuoc2);
-    cout << "Nhập mảng thứ hai: ";
-    cin >> arr2;
+    std::cout << "Nhập mảng thứ hai: ";
+    std::cin >> arr2;
 
     MangSoNguyen arr3 = arr1 + arr2;
-    cout << "Tổng của hai mảng là: " << arr3 << std::endl;
+    std::cout << "Tổng của hai mảng là: " << arr3 << std::endl;
 
     ++arr1;
-    cout << "Mảng thu 1 khi tăng mỗi phần tử lên 1 đơn vị: " << arr1 << std::endl;
+    std::cout << "Mảng thứ nhất sau khi tăng mỗi phần tử lên 1 đơn vị: " << arr1 << std::endl;
+
     return 0;
 }
